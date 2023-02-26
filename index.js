8f39043475245330caf5f861d8cd027e2e23091f
PAVAN LENKALA <l.pavan6445@gmail.com>
2023-02-26 23:26:59 +0530

    Updates

--- index.js
+++ index.js
@@ -104,13 +104,14 @@ MY_PROJECTS.forEach(({
     <div class="sc-crrszt efwbxD">
       <h3>${title}</h3>
       <div class="sc-bqyKOL jtIpxz">
-        <a href="https://github.com/Lpavan6445/Cult.fit-clone" target="_blank" rel="noopener noreferrer">
+        <a href=${git_hub_link} target="_blank" rel="noopener noreferrer">
           <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
             <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
             </path>
           </svg>
         </a>
         <a href="https://cult-fit-clone-w13.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+        <a href=${live_link} target="_blank" rel="noopener noreferrer">
           <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" class="icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M1.5 13A1.5 1.5 0 003 14.5h8a1.5 1.5 0 001.5-1.5V9a.5.5 0 00-1 0v4a.5.5 0 01-.5.5H3a.5.5 0 01-.5-.5V5a.5.5 0 01.5-.5h4a.5.5 0 000-1H3A1.5 1.5 0 001.5 5v8zm7-11a.5.5 0 01.5-.5h5a.5.5 0 01.5.5v5a.5.5 0 01-1 0V2.5H9a.5.5 0 01-.5-.5z" clip-rule="evenodd">
             </path>
